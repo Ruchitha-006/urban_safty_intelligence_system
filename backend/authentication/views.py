4abba722e665fb76f14848b9f291a1b2fece4710
RUCHITHA <ruchuruchib@gmail.com>
2026-08-16 21:56:35 +0530

    backend/authentication/urls.py         modified:   backend/authentication/views.py         deleted:    backend/dashboard/forms.py         modified:   backend/dashboard/views.py

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,43 +1,65 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+
 from .forms import RegisterForm, LoginForm
 
 
 def landing_view(request):
-    return render(request, "authentication/index.html")
+    return render(
+        request,
+        "authentication/index.html"
+    )
 
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
 
-    form = LoginForm(request, data=request.POST or None)
+    form = LoginForm(
+        request,
+        data=request.POST or None
+    )
 
     if request.method == "POST":
         if form.is_valid():
-            login(request, form.get_user())
+            user = form.get_user()
+            login(request, user)
+
             return redirect("dashboard")
 
-    return render(request, "authentication/login.html", {"form": form})
+    return render(
+        request,
+        "authentication/login.html",
+        {"form": form}
+    )
 
 
 def register_view(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
 
-    form = RegisterForm(request.POST or None)
+    form = RegisterForm(
+        request.POST or None
+    )
 
     if request.method == "POST":
         if form.is_valid():
             user = form.save()
+
             login(request, user)
+
             return redirect("dashboard")
 
-    return render(request, "authentication/register.html", {"form": form})
+    return render(
+        request,
+        "authentication/register.html",
+        {"form": form}
+    )
 
 
 @login_required
 def logout_view(request):
     logout(request)
+
     return redirect("landing")
